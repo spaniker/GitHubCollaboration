3406bd94ed02e4808b63b1c6d75feb2b407205de
spaniker <84048769+spaniker@users.noreply.github.com>
2025-08-01 10:21:08 -0700

    Fixed the homepage

---
+++ GitHubCollaboration/five.swift
@@ -0,0 +1,31 @@
+//
+//  five.swift
+//  GitHubCollaboration
+//
+//  Created by Syona Paniker on 8/1/25.
+//
+
+import SwiftUI
+struct five: View {
+  var body: some View {
+    Text("""
+       FOUNDATION CHAIN: Ch 41(46, 51, 56, 61, 66, 71).
+       Crochet chains until you reach the desired length for your top.
+       ROW 1: HDC into 2nd ch from hook, HDC in each st until end, turn.
+       ROW 2: Ch 1, BLO HDC in each st until last st, HDC in last st, turn.
+      Repeat ROW 2 until your panel length = waist measurement.
+      Wrap your panel around your body.
+      If the edges reach in the middle of your bust and you like the fit, fasten off leaving a tail of yarn (approx. 2 times longer than your foundation chain).
+      If you want your top to be looser, add repeats of ROW 2.
+            If you want to have a tighter top, remove repeats of ROW 2 until you reach your desired ease.
+            Fasten off, leaving a tail of yarn (approx. 2 times longer than your foundation chain).
+            Sew the ends of your body panel together. For a more ‘seamless’ look, only sew into the back loops
+            Try on your top and using stitch markers or safety pins, mark where you would like your straps to fall.
+            Single crochet two straps until desired length is reached and stich into the back panel
+      """)
+    Spacer()
+  }
+}
+#Preview {
+  five()
+}
